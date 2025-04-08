ed2e94f40ad1f2f6294b8da11253c901c7242ed6
Almamatova <zunusovbaaman@gmail.com>
2025-04-08 16:35:23 +0300

    Add files via upload

---
+++ task_str.py
@@ -0,0 +1,46 @@
+ # Задача 1
+# volume_d = 1.44 * 1024 * 1024
+# page = 100
+# line = 50
+# simbol = 25
+# byte = 4
+# volume_b = page * line * simbol * byte
+# numbook = volume_d // volume_b
+# print(numbook)
+
+
+#   # Задача 2
+# pi =  3.1415
+# s = 5
+# r = 5
+# area_с = pi * r ** 2
+# length = 2 * pi * r
+# perimeter = 4 * s
+# area_s = s ** 2
+# print(round(area_с, 2))
+# print(area_s, )
+# print(round(length,2))
+# print(perimeter)
+
+#  # Задача 3
+# a = '0'
+# b = '1'
+# c = '2'
+# str = (20 * '0') + (50 * '1') + (30 * '2')
+# print(str)
+
+#   # Задача 4
+# speed = 4096 / 1024
+# time = 120 * 60
+# coast = 0.125
+# free = 500
+# file = speed * time
+# money = (file-free)*coast
+# print(file)
+# print(money)
+
+#   # Задача 5
+# length = 90
+# width = 50
+# perimeter = 2*(length + width)
+# print(perimeter)
